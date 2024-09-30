2939f83ecc2840bc981021a0b38079c01f7060a8
Daria Ignatieva <DariTheDragon@yandex.ru>
2024-09-30 15:48:28 +0300

    The validation has been made

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ const addStatus = (status, input) => {
     }
 }
 
+const isPasswordsMatch = (password, password_confirm) => {
+    return password === password_confirm;
+}
+
 const form = document.getElementById('form');
 const first_name = document.getElementById("first-name");
 const last_name = document.getElementById("last-name");
@@ -16,8 +20,9 @@ const password = document.getElementById("password");
 const password_confirm = document.getElementById("password-confirm");
 const birth_day = document.getElementById("birth-day");
 const inputs = Array.prototype.slice.call(document.getElementsByTagName('input'));
+const submit_btn = document.getElementById("form-button");
 
-first_name.addEventListener('focusout', (event) => {
+first_name.addEventListener('input', (event) => {
     const input = event.target
     const userName = input.value.trim().toLowerCase();
     const status = (/^[a-zA-Zа-яёА-ЯЁ]+$/).test(userName)
@@ -25,7 +30,7 @@ first_name.addEventListener('focusout', (event) => {
     addStatus(status, input);
 });
 
-last_name.addEventListener('focusout', (event) => {
+last_name.addEventListener('input', (event) => {
     const input = event.target
     const userSurname = input.value.trim().toLowerCase();
     const status = (/^[a-zA-Zа-яёА-ЯЁ]+$/).test(userSurname)
@@ -33,7 +38,7 @@ last_name.addEventListener('focusout', (event) => {
     addStatus(status, input);
 });
 
-email.addEventListener('focusout', (event) => {
+email.addEventListener('input', (event) => {
     const input = event.target
     const userEmail = input.value.trim().toLowerCase();
     const status = (/^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu).test(userEmail)
@@ -41,15 +46,18 @@ email.addEventListener('focusout', (event) => {
     addStatus(status, input);
 })
 
-password.addEventListener('focusout', (event) => {
+password.addEventListener('input', (event) => {
     const input = event.target
     const userPassword = input.value.trim();
+    const userPasswordConfirm = password_confirm.value.trim();
     const status = (/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9]{8,20}$/).test(userPassword)
+    const statusForConfirm = userPassword === userPasswordConfirm;
 
     addStatus(status, input);
+    addStatus(statusForConfirm, password_confirm);
 })
 
-password_confirm.addEventListener('focusout', (event) => {
+password_confirm.addEventListener('input', (event) => {
     const input = event.target
     const userPassword = password.value.trim();
     const userPasswordConfirm = input.value.trim();
@@ -58,20 +66,18 @@ password_confirm.addEventListener('focusout', (event) => {
     addStatus(status, input);
 })
 
-birth_day.addEventListener('focusout', (event) => {
+birth_day.addEventListener('input', (event) => {
     const input = event.target
     const userBirthDay = birth_day.value.trim();
-    console.log(userBirthDay)
     const status = (/^[0-9]{4}-[0-9]{2}-[0-9]{2}$/).test(userBirthDay);
     addStatus(status, input);
 })
 
-form.addEventListener('focusout', (event) => {
+form.addEventListener('input', (event) => {
     event.preventDefault();
 
     let isOllValid = true;
 
-    console.log(inputs);
     inputs.forEach((item) => {
         if (!item.classList.contains('valid')) {
             isOllValid = false;
@@ -79,4 +85,10 @@ form.addEventListener('focusout', (event) => {
     })
 
     addStatus(isOllValid, form);
+
+    if (form.classList.contains('valid')) {
+        submit_btn.disabled = false;
+    } else {
+        submit_btn.disabled = true;
+    }
 });
